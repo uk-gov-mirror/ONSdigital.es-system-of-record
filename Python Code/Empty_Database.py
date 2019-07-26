3559aedf42b3e0a967d12a990c8f4c3c708c4599
Jamie Howells <jim-74@live.com>
2019-07-26 14:23:57 +0100

    Added Empty_Database.py file

---
+++ Python Code/Empty_Database.py
@@ -0,0 +1,24 @@
+"""Blank Schema
+
+Revision ID: Empty_Database
+Revises: 
+Create Date: 2019-07-23 12:00:27.755804
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'Empty_Database'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
