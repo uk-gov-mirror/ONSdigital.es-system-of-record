fabecdae267f4d61025a821a05b6dcbc456cb2af
Jamie Howells <jim-74@live.com>
2019-08-05 11:56:04 +0100

    findQuery now works after adding getattr

--- findQuery.py
+++ findQuery.py
@@ -2,7 +2,7 @@ from marshmallow import ValidationError
 import ioValidation
 import json
 import psycopg2
-import pandas.io.sql as psql
+import pandas as pd
 import os
 import sqlalchemy as db
 from sqlalchemy.orm import Session
@@ -12,10 +12,10 @@ import alchemy_functions
 def lambda_handler(event, context):
     database = os.environ['Database_Location']
 
-    try:
-        result = ioValidation.QuerySearch(strict=True).load(event)
-    except ValidationError as err:
-        return err.messages
+#    try:
+#        result = ioValidation.QuerySearch(strict=True).load(event)
+#    except ValidationError as err:
+#        return err.messages
 
     search_list = ['query_reference',
                    'survey_period',
@@ -32,9 +32,10 @@ def lambda_handler(event, context):
         return json.loads('{"contributor_name":"Failed To Connect To Database."}')
 
 
-#    all_query_sql = "SELECT * FROM es_db_test.Query WHERE"
+
     table_model = alchemy_functions.table_model(engine, metadata, "query")
     all_query_sql = db.select([table_model])
+
     added_query_sql = 0
 
     for criteria in search_list:
@@ -43,13 +44,13 @@ def lambda_handler(event, context):
         if event[criteria] == "":
             continue
         added_query_sql += 1
-        all_query_sql = all_query_sql.where(table_model.columns.criteria == event[criteria])
+        all_query_sql = all_query_sql.where(getattr(table_model.columns,criteria) == event[criteria])
+        print(all_query_sql)
 
     if added_query_sql == 0:
         all_query_sql = all_query_sql.where(table_model.columns.query_status == 'Open')
 
     query = alchemy_functions.select(all_query_sql, session)
-    #query = psql.read_sql(all_query_sql, connection, params={'QueryReference': event['QueryReference'], 'PeriodQueryRelates': event['PeriodQueryRelates'], 'QueryType': event['QueryType'], 'RUReference': event['RUReference'], 'SurveyOutputCode': event['SurveyOutputCode'], 'QueryStatus': event['QueryStatus']})
 
     table_list = {'step_exception': None,
                   'question_anomaly': None,
@@ -67,41 +68,35 @@ def lambda_handler(event, context):
         Period = str(curr_query['survey_period'].iloc[0])
         Survey = curr_query['survey_code'].iloc[0]
 
-#        try:
+        try:
 
-        for current_table in table_list:
-            table_model = alchemy_functions.table_model(engine, metadata, current_table)
+            for current_table in table_list:
+                table_model = alchemy_functions.table_model(engine, metadata, current_table)
 
-            # Can't use a single select for the 5 tables as two use different criteria. Will meed to change.
-            if current_table == 'step_exception' || current_table == 'query_task' || current_table == 'query_task_update':
-                statement = db.select([table_model]).where(table_model.columns.query_reference == Ref)
-            else:
-                statement = db.select([table_model]).where(db.and_(table_model.columns.survey_period == Period,table_model.columns.survey_code == Survey,table_model.columns.ru_reference == RU))
+                # Can't use a single select for the 5 tables as two use different criteria. Will meed to change.
+                if (current_table == 'step_exception') or (current_table == 'query_task')\
+                        or (current_table == 'query_task_update'):
+                    statement = db.select([table_model]).where(table_model.columns.query_reference == Ref)
+                else:
+                    statement = db.select([table_model]).where(db.and_(table_model.columns.survey_period == Period,table_model.columns.survey_code == Survey,table_model.columns.ru_reference == RU))
 
-            if current_table == "failed_vet":
-                other_model = alchemy_functions.table_model(engine, metadata, "vet")
-                joined = table_model.join(other_model)
-                statement = statement.select_from(joined)
+                if current_table == "failed_vet":
+                    other_model = alchemy_functions.table_model(engine, metadata, "vet")
+                    joined = table_model.join(other_model)
+                    statement = statement.select_from(joined)
 
-            table_data = alchemy_functions.select(statement, session)
-            table_list[current_table] = table_data
+                table_data = alchemy_functions.select(statement, session)
+                table_list[current_table] = table_data
 
-#            step_exceptions = psql.read_sql("SELECT * FROM es_db_test.Step_Exception WHERE QueryReference = %(Ref)s;", connection, params={'Ref': Ref})
-#            question_anomaly = psql.read_sql("SELECT * FROM es_db_test.Question_Anomaly WHERE SurveyPeriod = %(Period)s AND RUReference = %(RU)s AND SurveyOutputCode = %(Survey)s;", connection, params={'RU': RU, 'Period': Period, 'Survey': Survey})
-#            VETs = psql.read_sql("SELECT a.*,b.Description FROM es_db_test.Failed_VET a, es_db_test.VET b WHERE a.SurveyPeriod = %(Period)s AND a.RUReference = %(RU)s AND a.SurveyOutputCode = %(Survey)s AND a.FailedVET = b.VET;", connection, params={'RU': RU, 'Period': Period, 'Survey': Survey})
-#            query_tasks = psql.read_sql("SELECT * FROM es_db_test.Query_Task WHERE QueryReference = %(Ref)s;", connection, params={'Ref': Ref})
-#            query_task_updates = psql.read_sql("SELECT * FROM es_db_test.Query_Task_Update WHERE QueryReference = %(Ref)s;", connection, params={'Ref': Ref})
-#        except:
-#            return json.loads('{"queryreference":"Error","querytype":"Error selecting query from database"}')
+        except:
+            return json.loads('{"queryreference":"Error","querytype":"Error selecting query from database"}')
 
         curr_query = json.dumps(curr_query.to_dict(orient='records'), sort_keys=True, default=str)
         curr_query = curr_query[1:-2]
         outJSON += curr_query + ',"Exceptions":[ '
-        for index, step_row in table_list['step_exceptions'].iterrows():
+        for index, step_row in table_list['step_exception'].iterrows():
             row_step = step_row['step']
-            curr_step = table_list['step_exceptions']['step'] == \
-                        (row_step & (table_list['step_exceptions']['run_id'] ==
-                                     step_row['run_id']))
+            curr_step = table_list['step_exception'][(table_list['step_exception']['step'] == row_step) & (table_list['step_exception']['run_id'] == step_row['run_id'])]
             if curr_step.empty:
                 continue
 
@@ -109,9 +104,9 @@ def lambda_handler(event, context):
             curr_step = curr_step[1:-2]
             outJSON += (curr_step + ',"Anomalies":[ ')
             for index, ano_row in table_list['question_anomaly'].iterrows():
-                curr_ano = table_list['question_anomaly'](table_list['question_anomaly']['step'] == ano_row['step']) \
+                curr_ano = table_list['question_anomaly'][(table_list['question_anomaly']['step'] == ano_row['step']) \
                            & (table_list['question_anomaly']['question_number'] == ano_row['question_number']) & \
-                           (table_list['question_anomaly']['step'] == row_step)
+                           (table_list['question_anomaly']['step'] == row_step)]
                 if curr_ano.empty:
                     continue
 
@@ -139,12 +134,15 @@ def lambda_handler(event, context):
             curr_tas = curr_tas[1:-2]
             outJSON += curr_tas
             outJSON = outJSON + ',"QueryTaskUpdates":'
-            curr_up = table_list['query_task_update'][(table_list['query_task_update']['query_reference']
-                                                        == tas_row['query_reference']) &
-                                                       (table_list['query_task_update']['task_sequence_number'] ==
-                                                        tas_row['task_sequence_number'])]
+            if not table_list['query_task_update'].empty:
+                curr_up = table_list['query_task_update'][(table_list['query_task_update']['query_reference']
+                                                            == tas_row['query_reference']) &
+                                                           (table_list['query_task_update']['task_sequence_number'] ==
+                                                            tas_row['task_sequence_number'])]
 
-            curr_up = json.dumps(curr_up.to_dict(orient='records'), sort_keys=True, default=str)
+                curr_up = json.dumps(curr_up.to_dict(orient='records'), sort_keys=True, default=str)
+            else:
+                curr_up = "[]"
             outJSON += curr_up + '},'
 
         outJSON = outJSON[:-1]
